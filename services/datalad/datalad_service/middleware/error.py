8238506c44e3997feccae21d1b6d03e7f6494eb1
Ross Blair <rosswilsonblair@gmail.com>
2026-03-23 09:32:24 -0500

    add logging line to end of uncaught exception in datalad_service to confirm req and resp are empty.

--- services/datalad/datalad_service/middleware/error.py
+++ services/datalad/datalad_service/middleware/error.py
@@ -11,5 +11,7 @@ class CustomErrorHandlerMiddleware:
                 logging.exception(f'Unhandled exception: {exc}', exc_info=eval(exc))
             else:
                 logging.error('Request failed without exception details')
+                logging.error(resource)
                 logging.error(req)
                 logging.error(resp)
+                logging.error('End Request failed without exception details')
